17204c51444afb16873cc0049913219fb3590fa8
smlopezsm <santilm.sjlm@gmail.com>
2023-02-22 19:38:18 -0300

    frst cmmt

---
+++ index.js
@@ -0,0 +1,8 @@
+
+document.getElementById('boton').onclick = function () {
+  window.location.href = "dooms page";
+}
+
+document.getElementById('boton-dos').onclick = function () {
+  window.location.href = "instagram fake login";
+}
